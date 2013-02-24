b2235d05b10804ccd71105c51b0657a1ab8dac88
hypercross <helios.tnak@gmail.com>
2013-02-24 01:54:24 -0500

    remove buttons upon start

--- JSanguosha/src/application/view/ConnectionView.java
+++ JSanguosha/src/application/view/ConnectionView.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 import application.network.NetworkManager;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -15,40 +16,50 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 public class ConnectionView extends Table{
 	public ConnectionView()
 	{
-		Button btn = new Button(new Label("run host",DefaultSkin.instance), DefaultSkin.instance);
-		this.add(btn);
-		btn.addListener(new ClickListener()
+		final Button btn0 = new Button(new Label("run host",DefaultSkin.instance), DefaultSkin.instance);
+		final Button btn1 = new Button(new Label("run server",DefaultSkin.instance), DefaultSkin.instance);
+		final Button btn2 = new Button(new Label("run client",DefaultSkin.instance), DefaultSkin.instance);
+		
+		this.add(btn0);
+		btn0.addListener(new ClickListener()
 		{
 			public void clicked (InputEvent event, float x, float y) {
 				try {
 					NetworkManager.instance.startHost();
+					btn0.remove();
+					btn1.remove();
+					btn2.remove();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
 		});
 		
-		btn = new Button(new Label("run server",DefaultSkin.instance), DefaultSkin.instance);
-		this.add(btn);
-		btn.addListener(new ClickListener()
+		this.add(btn1);
+		btn1.addListener(new ClickListener()
 		{
 			public void clicked (InputEvent event, float x, float y) {
 				try {
 					NetworkManager.instance.startServer();
+					btn0.remove();
+					btn1.remove();
+					btn2.remove();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
 		});
 		
-		btn = new Button(new Label("run client",DefaultSkin.instance), DefaultSkin.instance);
-		this.add(btn);
-		btn.addListener(new ClickListener()
+		this.add(btn2);
+		btn2.addListener(new ClickListener()
 		{
 			public void clicked (InputEvent event, float x, float y) {
 				try {
 					NetworkManager.instance.startClient("localhost");
 					Entity.ingame.clear();
+					btn0.remove();
+					btn1.remove();
+					btn2.remove();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
